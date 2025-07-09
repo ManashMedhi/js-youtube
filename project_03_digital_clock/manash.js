ac48d968390d8e07018614d27d87df64acd88df3
ManashMedhi <manashmedhi042@gmail.com>
2025-07-09 13:30:37 +0530

    project_on_setInterval

--- project_03_digital_clock/manash.js
+++ project_03_digital_clock/manash.js
@@ -1,1 +1,11 @@
 const clock = document.querySelector('#clock');
+
+let date = new Date()
+console.log(date.toLocaleTimeString()); // ai line ta amar nalakhilao kotha nai amar khali print koribo console ot
+// aikhine loike amar hoise time ta console ot ahiya ase 
+// ala amak html dia juta interface banising ote ahiba lage
+// so ami eta use korim
+setInterval(function(){
+    clock.innerHMTL = date.toLocaleTimeString();
+
+},1000);
